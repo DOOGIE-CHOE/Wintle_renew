c11e0c0eb1412e3ccb519506534799a27297ae67
Daniel Choi <cydp0127@gmail.com>
2016-06-01 17:47:52 +0900

    add secure session
    add secure session

---
+++ www/functions.php
@@ -0,0 +1,96 @@
+<?php
+include_once '../psl-config.php';
+
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 6/1/2016
+ * Time: 5:13 PM
+ */
+function sec_session_start() {
+    $session_name = 'wintle_session';   // Set a custom session name
+    $secure = true;
+
+    // This stops JavaScript being able to access the session id.
+    $httponly = true;
+
+    // Forces sessions to only use cookies.
+    if (ini_set('session.use_only_cookies', 1) === FALSE) {
+        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+        exit();
+    }
+
+    // Gets current cookies params.
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params($cookieParams["lifetime"],
+        $cookieParams["path"],
+        $cookieParams["domain"],
+        $secure,
+        $httponly);
+
+    // Sets the session name to the one set above.
+    session_name($session_name);
+    session_start();            // Start the PHP session
+    session_regenerate_id(true);    // regenerated the session, delete the old one.
+}
+
+function login($email, $password) {
+    // Using prepared statements means that SQL injection is not possible.
+    if ($stmt = $mysqli->prepare("SELECT id, username, password 
+        FROM members
+       WHERE email = ?
+        LIMIT 1")) {
+        $stmt->bind_param('s', $email);  // Bind "$email" to parameter.
+        $stmt->execute();    // Execute the prepared query.
+        $stmt->store_result();
+
+        // get variables from result.
+        $stmt->bind_result($user_id, $username, $db_password);
+        $stmt->fetch();
+
+        if ($stmt->num_rows == 1) {
+            // If the user exists we check if the account is locked
+            // from too many login attempts
+
+            if (checkbrute($user_id, $mysqli) == true) {
+                // Account is locked
+                // Send an email to user saying their account is locked
+                return false;
+            } else {
+                // Check if the password in the database matches
+                // the password the user submitted. We are using
+                // the password_verify function to avoid timing attacks.
+                if (password_verify($password, $db_password)) {
+                    // Password is correct!
+                    // Get the user-agent string of the user.
+                    $user_browser = $_SERVER['HTTP_USER_AGENT'];
+                    // XSS protection as we might print this value
+                    $user_id = preg_replace("/[^0-9]+/", "", $user_id);
+                    $_SESSION['user_id'] = $user_id;
+                    // XSS protection as we might print this value
+                    $username = preg_replace("/[^a-zA-Z0-9_\-]+/",
+                        "",
+                        $username);
+                    $_SESSION['username'] = $username;
+                    $_SESSION['login_string'] = hash('sha512',
+                        $db_password . $user_browser);
+                    // Login successful.
+                    return true;
+                } else {
+                    // Password is not correct
+                    // We record this attempt in the database
+                    $now = time();
+                    $mysqli->query("INSERT INTO login_attempts(user_id, time)
+                                    VALUES ('$user_id', '$now')");
+                    return false;
+                }
+            }
+        } else {
+            // No user exists.
+            return false;
+        }
+    }
+}
+
+?>
+
